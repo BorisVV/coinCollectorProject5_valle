1dc922612a645550287909570cccd3c62214ca8d
clara <clara@clara.com>
2017-04-13 12:21:21 -0500

    csv to sqlite

---
+++ csv_to_sql.py
@@ -0,0 +1,17 @@
+# Open CSV file
+
+# Open sqlite database
+
+# for every line in CSV file, write a row to database
+
+ # Table name: coins_web_app_displayquaters
+# Column names: same as variable names in model
+# Generate id, or is the table set up to autoincrement?
+
+0|id|integer|1||1
+1|number|integer|1||0
+2|release_date|varchar(200)|1||0
+3|elements|varchar(200)|1||0
+4|engraver|varchar(200)|1||0
+5|link|text|1||0
+6|state|varchar(200)|1||0
